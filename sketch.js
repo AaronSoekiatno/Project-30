8ffa4473408445a11368e5766fa110a3d44011a3
FlashGod123 <67604319+FlashGod123@users.noreply.github.com>
2020-08-13 21:22:26 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,80 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+//row #1
+var box1,box2,box3;
+
+//row #2
+var box4,box5;
+
+//row #3
+var box6;
+
+var grnd,grnd2,sling,polygon,polygonImg;
+
+function preload(){
+  polygonImg = loadImage("hex.png");
+}
+
+function setup() {
+  createCanvas(800,400);
+
+  engine = Engine.create();
+  world = engine.world;
+
+  polygon = Bodies.rectangle(100,350,50,50);
+  World.add(world,polygon);
+  
+  grnd = new ground(400,390,800,20);
+  grnd2 = new ground(500,250,150,10);
+  sling = new slingshot(polygon,{x:100,y:250});
+
+  //row #1
+  box1 = new box(475,230,25,30);
+  box2 = new box(500,230,25,30);
+  box3 = new box(525,230,25,30);
+
+  //row #2
+  box4 = new box(488,198,25,30);
+  box5 = new box(513,198,25,30);
+
+  //row #3
+  box6 = new box(501,166,25,30);
+
+  Engine.run(engine);
+}
+
+function draw() {
+  background(150);  
+
+  imageMode(CENTER);
+  image(polygonImg,polygon.position.x,polygon.position.y,30,30);
+  
+  grnd.display();
+  grnd2.display();
+  sling.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(polygon, {x: mouseX , y: mouseY});
+}
+
+function mouseReleased(){
+  sling.fly();
+}
+
+function keyPressed(){
+  if(keyCode === 32){
+      sling.attach(polygon);
+  }
+  }
